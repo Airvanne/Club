8aac6f16b31775b25d68c71b8a2bbf37bccb856a
Airvanne <erwanb@lilo.org>
2018-11-06 01:51:44 +0100

    fail

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,16 +11,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
    assert flash.empty?
   end
 
-  test "good link or not in header" do
+#test bons liens sur page d'acceuil
+  test "good link or not in home" do
     User.new(first_name: 'Toto', last_name: 'To', email: 'toto@gmail.com', password: 'foobar').save
     get root_path
     session[:user_id] = 980190962
     if assert !User.find(session[:user_id]).nil?
+      assert_select "a[href=?]", login_path
+      assert_select "a[href=?]", new_user_path
+      assert_select "a[href=?]", root_path
+    end
+  end
 
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", new_user_path
-    assert_select "a[href=?]", root_path
-end
-
+  #Test bons liens sur navbar
+    test "navbar links" do
+      get root_path
+   		assert_select "a[href=?]", login_path
+      assert_select "a[href=?]", new_user_path
+      assert_select "a[href=?]", root_path
   end
 end
---
+++ test/system/sessions_test.rb
@@ -0,0 +1,8 @@
+require 'application_system_test_case'
+include BCrypt
+
+class SessionsTest < ApplicationSystemTestCase
+
+  
+
+end
